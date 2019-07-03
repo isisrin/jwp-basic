1196509d597626a288da62e85add79b5e3b63325
isisrin <isisrin@nate.com>
2019-07-03 20:23:13 +0900

    코드 리팩토링

--- src/main/java/next/controller/DispatcherServlet.java
+++ src/main/java/next/controller/DispatcherServlet.java
@@ -12,35 +12,33 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 @WebServlet(urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         log.info("리퀘스트 URI {} /GET", req.getRequestURI());
-        test(req, resp);
-
+        requestProcessor(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         log.info("리퀘스트 URI {} /POST", req.getRequestURI());
-        test(req, resp);
+        requestProcessor(req, resp);
     }
 
     @SneakyThrows
-    private void test(HttpServletRequest req, HttpServletResponse resp) {
+    private void requestProcessor(HttpServletRequest req, HttpServletResponse resp) {
         String requestURI = req.getRequestURI();
-        Controller huhu = RequestMapping.getMatchedController(requestURI);
-        if(huhu == null) {
+        Controller matchedController = RequestMapping.getMatchedController(requestURI);
+        if(matchedController == null) {
             resp.sendRedirect(requestURI);
             return;
         }
-        String hoho = RequestMapping.getMatchedController(requestURI).service(req, resp);
-        if(hoho.contains(".jsp")) {
-            RequestDispatcher rd = req.getRequestDispatcher(hoho);
+        String redirectUrl = RequestMapping.getMatchedController(requestURI).service(req, resp);
+        if(redirectUrl.contains(".jsp")) {
+            RequestDispatcher rd = req.getRequestDispatcher(redirectUrl);
             rd.forward(req, resp);
             return;
         }
-        resp.sendRedirect(hoho);
+        resp.sendRedirect(redirectUrl);
     }
 }
